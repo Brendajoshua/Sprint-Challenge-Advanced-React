e1e425b1904d183dcb86358dc56ddc331971f121
Brenda Joshua <brendajosh.bj@gmail.com>
2019-10-04 10:47:17 +0300

    built the App class component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      players: [],
+    }
+  }
+
+  componentDidMount(){
+    axios
+    .get('http://localhost:5000/api/players')
+    .then(response => {
+      this.setState({players: response.data})
+    })
+    .catch(error => console.log(error))
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <h1>App</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
